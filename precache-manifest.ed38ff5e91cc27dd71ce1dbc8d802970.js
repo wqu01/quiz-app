6266d8a79041a3eba75f2e5baa348b5719f83cb0
Wenniao Q <wenniao@wenniaos-mbp.myfiosgateway.com>
2020-10-13 20:22:58 -0400

    Updates

---
+++ precache-manifest.ed38ff5e91cc27dd71ce1dbc8d802970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd48546090bdc14ae6074f6efafaf1c5",
+    "url": "/wqu01/quiz-app.git/index.html"
+  },
+  {
+    "revision": "22154be38f743c3fec02",
+    "url": "/wqu01/quiz-app.git/static/css/main.ddd319b6.chunk.css"
+  },
+  {
+    "revision": "faeefc122056d0923048",
+    "url": "/wqu01/quiz-app.git/static/js/2.b8719b33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wqu01/quiz-app.git/static/js/2.b8719b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22154be38f743c3fec02",
+    "url": "/wqu01/quiz-app.git/static/js/main.bcc67c80.chunk.js"
+  },
+  {
+    "revision": "406fb5afb167f4a76ddf",
+    "url": "/wqu01/quiz-app.git/static/js/runtime-main.333234a5.js"
+  }
+]);
